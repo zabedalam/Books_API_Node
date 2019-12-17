0d5b4c97fb31be8a1f5536d88fa16efc8f56e397
zabed <zabedalam777@gmail.com>
2019-12-17 17:15:29 +0100

    test

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const booksRouter=require("./src/books")
+const commentsRouter=require("./src/comments")
+const server= express()
+
+const port = process.env.PORT || 4000
+
+server.use(express.json())
+
+server.use("/books",booksRouter)
+
+server.listen(port,()=>{
+    console.log("I am listening on port" + port)
+})
+
---
+++ src/comments/index.js
@@ -0,0 +1,20 @@
+const express =require("express")
+
+
+const router=express.Router()
+
+router.get("/:id",async(req,res)=>{
+
+})
+
+router.post("/:id",async(req,res)=>{
+
+})
+
+router.put("/:id",async(req,res)=>{
+
+})
+
+router.delete("/:id",async(req,res)=>{
+    
+})
