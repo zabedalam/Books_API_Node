9cf8b3f0f1a2e6c4395d68a9ff90a6b493ef755c
zabed <zabedalam777@gmail.com>
2019-12-19 13:45:57 +0100

    customers added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const booksRouter=require("./src/books")
 const commentsRouter=require("./src/comments")
+const customerRouter=require("./src/customers")
 const server= express()
 
 const port = process.env.PORT || 4000
@@ -10,6 +11,7 @@ server.use(express.json())
 console.log(port)
 
 server.use("/books",booksRouter)
+server.use("/customers",customerRouter)
 
 server.listen(port,()=>{
     console.log("I am listening on port" + port)
--- src/comments/index.js
+++ src/comments/index.js
@@ -18,3 +18,5 @@ router.put("/:id",async(req,res)=>{
 router.delete("/:id",async(req,res)=>{
     
 })
+
+module.exports=router
